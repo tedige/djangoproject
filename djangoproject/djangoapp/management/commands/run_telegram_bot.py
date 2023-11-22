dc5553c9f238475c0f071200dd8d8a7199970aee
tedige <114273306+tedige@users.noreply.github.com>
2023-11-22 13:05:48 +0600

    Update run_telegram_bot.py

--- djangoproject/djangoapp/management/commands/run_telegram_bot.py
+++ djangoproject/djangoapp/management/commands/run_telegram_bot.py
@@ -1,37 +1,29 @@
 from django.core.management.base import BaseCommand
 import telebot
-from djangoapp.models import Product
+from djangoapp.models import User
 
-bot = telebot.TeleBot("6641758955:AAG0Gtq0-5FAttlE3bwLsCQImPCNLkZ_6jQ")
+TOKEN = "6641758955:AAG0Gtq0-5FAttlE3bwLsCQImPCNLkZ_6jQ"
+bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Hello world!")
+    bot.send_message(message.chat.id, "Hello! Welcome to the bot!")
 
-@bot.message_handler(commands=['products'])
-def products(message):
-    products = Product.objects.all()
-    for product in products:
-        bot.send_message(message.chat.id, f"{product.name} - {product.price}")
-
-@bot.message_handler(commands=['help'])
-def help(message):
-    bot.send_message(message.chat.id, "Список доступных команд:\n/products - Показать список товаров\n/add <название_товара> <цена> - Добавить товар")
+@bot.message_handler(commands=['users'])
+def show_users(message):
+    users = User.objects.all()
+    user_list = "\n".join([f"@{user.username}" for user in users])
+    bot.send_message(message.chat.id, f"Users:\n{user_list}")
 
 @bot.message_handler(commands=['add'])
-def add_product(message):
+def add_user(message):
     try:
-        command, *args = message.text.split()  # Разделяем команду и аргументы
-        if len(args) != 2:
-            bot.send_message(message.chat.id, "Неверный формат. Используйте: /add <название_товара> <цена>")
-        else:
-            product_name, product_price = args
-            new_product = Product(name=product_name, price=product_price)
-            new_product.save()
-            bot.send_message(message.chat.id, f"Товар '{product_name}' добавлен успешно.")
-    except Exception as e:
-        bot.send_message(message.chat.id, f"Произошла ошибка: {e}")
-
+        command, username, telegram_id = message.text.split()
+        telegram_id = int(telegram_id)
+        new_user = User.objects.create(username=username, telegram_id=telegram_id)
+        bot.send_message(message.chat.id, f"Added user: {new_user.username}")
+    except ValueError:
+        bot.send_message(message.chat.id, "Invalid input! Use: /add <username> <telegram_id>")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
