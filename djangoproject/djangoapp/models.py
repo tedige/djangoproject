78b22ab5b898500b4d9372c6028069d4c2449dc3
tedige <114273306+tedige@users.noreply.github.com>
2023-11-22 13:06:40 +0600

    Update models.py

--- djangoproject/djangoapp/models.py
+++ djangoproject/djangoapp/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+class User(models.Model):
+    telegram_id = models.IntegerField(unique=True)
+    username = models.CharField(max_length=255)
 
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.IntegerField()
 
+    def __str__(self):
+        return self.username
