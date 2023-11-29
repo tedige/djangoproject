f1c7bcc1dda00fbd331f779b3ae64675b4de35ca
Tomiris Edige <tomiedige@icloud.com>
2023-11-29 14:27:50 +0600

    fix

--- djangoproject/djangoapp/tests.py
+++ djangoproject/djangoapp/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+
+class UserModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create(telegram_id=123456, username='test_user')
+
+    def test_user_creation(self):
+        user_count = User.objects.count()
+        self.assertEqual(user_count, 1)
+
+    def test_user_username(self):
+        user = User.objects.get(telegram_id=123456)
+        self.assertEqual(user.username, 'test_user')
+
+    def test_user_instance(self):
+        user = User.objects.get(telegram_id=123456)
+        self.assertIsInstance(user, User)
